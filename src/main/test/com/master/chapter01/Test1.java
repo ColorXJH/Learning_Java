4d8420b21972a333d6b675713ce4d43c8d466382
Master <1204332316@qq.com>
2023-08-08 17:07:00 +0800

    300 --添加junit

---
+++ src/main/test/com/master/chapter01/Test1.java
@@ -0,0 +1,16 @@
+package com.master.chapter01;
+
+import org.junit.Test;
+
+/**
+ * @author ColorXJH
+ * @version 1.0
+ * @description:
+ * @date 2023-08-08 17:02
+ */
+public class Test1 {
+    @Test
+    public void test1(){
+
+    }
+}

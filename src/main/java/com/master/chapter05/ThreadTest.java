76b0e723d99b4b5c9e83c4b71ac407a8023bd79f
Master <1204332316@qq.com>
2023-08-18 16:43:40 +0800

    403 多线程

---
+++ src/main/java/com/master/chapter05/ThreadTest.java
@@ -0,0 +1,13 @@
+package com.master.chapter05;
+
+/**
+ * @author ColorXJH
+ * @version 1.0
+ * @description: 多线程案例
+ * @date 2023-08-18 16:42
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+
+    }
+}

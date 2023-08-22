d7ce7e2d995ded3345cc3f52e8b1dc279bac9467
Master <1204332316@qq.com>
2023-08-22 17:01:02 +0800

    422 多线程

--- src/main/java/com/master/chapter05/MyRunnable.java
+++ src/main/java/com/master/chapter05/MyRunnable.java
@@ -7,9 +7,57 @@ package com.master.chapter05;
  * @Datetime 2023/8/21 21:07
  * @author ColorXJH
  */
-public class MyRunnable implements Runnable{
+public class MyRunnable{
+    public static void main(String[] args) {
+//        MyThreads thread1=new MyThreads();
+//        MyThreads thread2=new MyThreads();
+//        MyThreads thread3=new MyThreads();
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+
+        MyRun r1=new MyRun();
+        Thread s1=new Thread(r1);
+        Thread s2=new Thread(r1);
+        Thread s3=new Thread(r1);
+        s1.start();
+        s2.start();
+        s3.start();
+
+    }
+}
+
+class MyThreads extends Thread{
+    //待解决的线程安全问题
+
+    private static  int ticket=100;
     @Override
     public void run() {
-        System.out.println("myRunnable");
+        while (true){
+            if(ticket>0){
+                System.out.println(Thread.currentThread().getName()+":ticket="+ticket);
+                ticket--;
+            }else{
+                break;
+            }
+        }
+
+    }
+}
+
+class MyRun implements Runnable{
+    //待解决的线程安全问题
+
+    private int ticket1=100;
+    @Override
+    public void run() {
+        while (true){
+            if(ticket1>0){
+                System.out.println(Thread.currentThread().getName()+":ticket="+ticket1);
+                ticket1--;
+            }else{
+                break;
+            }
+        }
     }
 }
--- src/main/java/com/master/chapter05/ThreadTest.java
+++ src/main/java/com/master/chapter05/ThreadTest.java
@@ -8,15 +8,33 @@ package com.master.chapter05;
  */
 public class ThreadTest {
     public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getPriority());
         MyThread myThread=new MyThread();
+        //方式1
+        myThread.setPriority(10);
         myThread.start();
         System.out.println("main start---");
+        //方式二
+        new Thread(){
+            @Override
+            public void run() {
+                System.out.println("创建类的匿名子类的匿名对象执行线程");
+                System.out.println(Thread.currentThread().getPriority());
+            }
+        }.start();
+        for (int i=0;i<100;i++){
+            System.out.println(Thread.currentThread().getName());
+        }
     }
 }
 
 class MyThread extends Thread{
     @Override
     public void run() {
+        System.out.println(Thread.currentThread().getPriority());
         System.out.println("thread start---");
+        for (int i=0;i<100;i++){
+            System.out.println(Thread.currentThread().getName());
+        }
     }
 }

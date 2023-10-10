2276d374e8331d6531a070edda61dc3773b00e85
ColorXJH <1204332316@qq.com>
2023-10-10 22:12:50 +0800

    638 java的反射： 可以动态的获取类的实例 属性 方法 构造器 通过Class类的实例得到运行时类

--- src/main/java/com/master/chapter14/ReflectTest.java
+++ src/main/java/com/master/chapter14/ReflectTest.java
@@ -2,6 +2,11 @@ package com.master.chapter14;
 
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @ClassName: ReflectTest
  * @Package: com.master.chapter14
@@ -25,6 +30,42 @@ public class ReflectTest {
         person.show();
         //在person类外部，不可以通过其对象 调用其内部私有的结构，比如私有方法，私有变量 私有的构造器，受到封装性的限制
     }
+    //反射之后，对person的操作
+    @Test
+    public void afterReflect() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        //1: 通过反射创建Person类的对象
+        //获取Class对象
+        Class<Person> personClass = Person.class;
+        //获取对象的构造器
+        Constructor<Person> constructor = personClass.getConstructor(String.class, int.class);
+        //构造对象
+        Person jerry = constructor.newInstance("Jerry", 28);
+        System.out.println(jerry);
+
+        //2: 通过反射，调用对象指定的属性、方法
+        Field age = personClass.getDeclaredField("age");
+        age.set(jerry,27);
+        System.out.println(jerry);
+
+        Method show = personClass.getDeclaredMethod("show");
+        show.invoke(jerry);
+        //3: 通过反射可以调用Person类的私有方法 属性 构造器
+        Method showNation = personClass.getDeclaredMethod("showNation", String.class);
+        //改变方法的访问范围
+        showNation.setAccessible(true);
+        Object china = showNation.invoke(jerry, "China");
+        System.out.println(china);
+        Field name = personClass.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(jerry,"ColorXJH");
+        System.out.println(jerry);
+        Constructor<Person> constructor1 = personClass. getDeclaredConstructor(String.class);
+        constructor1.setAccessible(true);
+        Person kcy_zyf = constructor1.newInstance("KCY_ZYF");
+        System.out.println(kcy_zyf);
+
+        //4:获取Class实例的4中方式
+    }
 }
 
 /**
@@ -46,4 +87,25 @@ public class ReflectTest {
  *                  java.lang.reflect.Method:代表类的方法
  *                  java.lang.reflect.Field:代表类的成员变量
  *                  java.lang.reflect.Constructor:代表类的构造器
+ */
+
+/**
+ *  疑问：通过直接new对象的方式或反射的方式都有可以调用公共的结构，开发中到底用哪个？
+ *      建议使用直接new的方式，什么时候会使用到反射的方式？
+ *             动态性，编译的时候无法确定使用哪个类的对象，就使用反射的方式
+ *                  例如：服务器代码跑起来了，根据请求url，动态创造不同的类的对象执行不同的方法
+ *  疑问：反射机制与面向对象中的封装性是不是矛盾的？如何看待两个技术？
+ *      不矛盾，封装性是解决建议使用哪些属性方法，反射是技术上的可实现性，动态性
+ */
+
+
+/**
+ * Class的理解 对于Person类有不同的实例对象
+ * 那么对于许多特定的类，抽象出来一个更高的层级，都有属性 构造器 方法，等
+ * 这些类就可以使用新的一个类:Class来表示，它表明每一个类都共有的一些属性
+ * 类的加载过程：java程序编写完成之后-》javac.exe（编译器编译过程，得到字节码文件）-》java.exe(java虚拟机JVM:解释执行过程,相当于将某个字节码文件加载到内存中)
+ *      此过程（加载的过程）被称为类的加载，加载到内存中的内就成为运行时类，此时的类就作为Class的一个实例，比如Person.class字节码文件加载到内存之后，其本身就充当Class类的一个实例
+ *          以前通过类去造对象-》现在类本身也是一个对象，是Class的对象 Class类是class类的更高一级抽象 将所有的class类本身抽象出来，得到一个新的类Class,注意大小写
+ *          换句话说：Class的实例就对应一个运行时类
+ *              万事万物皆对象：对象.xxx;File;URL;类本身也是对象=》反射;前端，数据库，在java层面都是对象
  */

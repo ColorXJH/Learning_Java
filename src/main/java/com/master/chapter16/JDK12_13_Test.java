9538552da1d3d897a5c2773451646e4ee4852b7d
Master <1204332316@qq.com>
2023-10-25 17:00:28 +0800

    740

--- src/main/java/com/master/chapter16/JDK12_13_Test.java
+++ src/main/java/com/master/chapter16/JDK12_13_Test.java
@@ -2,6 +2,12 @@ package com.master.chapter16;
 
 import org.junit.Test;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Optional;
 
 /**
@@ -33,7 +39,7 @@ public class JDK12_13_Test {
     }
 
     @Test
-    public void testSwitchOnJava13() throws IllegalAccessException {
+    public void testSwitchOnJava12() throws IllegalAccessException {
         String name = "Color";
         switch (name) {
             case "xjh" -> System.out.println("this is xjh");
@@ -60,6 +66,115 @@ public class JDK12_13_Test {
         Optional<String>optional=name.describeConstable();
         System.out.println(optional.get());
     }
+
+    @Test
+    public void testCompactNumberInstance(){
+        var cnf= NumberFormat.getCompactNumberInstance(Locale.CHINA,NumberFormat.Style.SHORT);
+        System.out.println(1_00000);
+        System.out.println(1_9200);
+        System.out.println(1_000_000);
+        System.out.println(1L<<30);
+        System.out.println(1L<<40);
+        System.out.println(1L<<50);
+        System.out.println("-----------------------");
+        System.out.println(cnf.format(1_000));
+        System.out.println(cnf.format(1_9200));
+        System.out.println(cnf.format(1_000_000));
+        System.out.println(cnf.format(1L<<30));
+        System.out.println(cnf.format(1L<<40));
+        System.out.println(cnf.format(1L<<50));
+
+    }
+
+    @Test
+    public void testStringAdd(){
+        var result="foo".transform(x->x+"bar");
+        System.out.println(result);
+        var test="bar".indent(2);
+        var test1="a\n   b\nc ".indent(2);
+        System.out.println(test);
+        System.out.println(test1);
+    }
+
+    @Test
+    public void testFilesMismatch() throws IOException {
+        //文件可以不存在，但是目录需要存在
+        FileWriter writer=new FileWriter("writer1.txt");
+        writer.write("a");
+        writer.write("b");
+        writer.write("c");
+        writer.write("d");
+        writer.close();
+        FileWriter writer2=new FileWriter("writer2.txt");
+        writer2.write("a");
+        writer2.write("u");
+        writer2.write("c");
+        writer2.write("d");
+        writer2.close();
+        //显示不匹配的数据的位置 从0开始，如果都匹配则返回-1
+        System.out.println(Files.mismatch(Path.of("writer1.txt"),Path.of("writer2.txt")));
+        /**
+         * IO:File
+         * NIO2:Files操作本地文件的工具类，Path:替换原有的File,Paths:实例化Path
+         *
+         */
+    }
+
+    @Test
+    public void testSwitchOnJava13(){
+        String x="color";
+        //->自带一个break的效果 :后面的操作 使用了yield就不需要使用break了
+        //:与->不可以混合使用
+        int i=switch (x){
+            case "color1"->5;
+            case "xjh"->6;
+            case "color"->{
+               yield 99;
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + x);
+        };
+        int y=switch (x){
+            case "color":yield 33;
+            case "xjh":yield 22;
+            default:yield 35;
+        };
+    }
+
+    @Test
+    public void testTextBlock(){
+        String x="<html>\n" +
+                "\t<p> this is colorxjh </p>\n" +
+                "</html>";
+        System.out.println(x);
+        System.out.println("------on java 13------");
+        //"""    """;之间的内容作为文本块
+        String textBlock= """
+                <html>
+                	<p> this is colorxjh </p>
+                </html>
+                """;
+        System.out.println(textBlock);
+        String names= """ 
+                abc\n
+                asdasdas\n
+                sdadasdsa\n""";
+        System.out.println(names);
+        String t1= """
+                abc""";
+        String t2= """
+                abc
+                """;
+        //表示空字符串
+        String t3= """
+                """;
+        //表示包含一个换行符
+        String t4= """
+                
+                """;
+        System.out.println(t1.length());//3
+        System.out.println(t2.length());//4  结束的地方包含了一个换行符
+        //其他的包括sql语句
+    }
 }
 
 
@@ -71,7 +186,7 @@ public class JDK12_13_Test {
  *      4：JVM常量API
  *      5：只保留一个AArch64实现
  *      6：默认类数据共享归档文件（cds:类数据共享 class data sharing）
- *      7：可终止的G1 Mixed GC
+ *      7：可终止的G1 Mixed GC (G1=gabage collection first,简称G1:垃圾优先回收机制)
  *      8：G1及时返回未使用的已分配内存
  *
  *          switch表达式
@@ -94,5 +209,40 @@ public class JDK12_13_Test {
  *          CDS:类数据共享机制：
  *              同一个物理机/虚拟机上启动多个jvm时，如果每一个jvm都单独装载所有的类，启动成本和占用内存将比较高，通过把一些核心类在每个jvm间共享
  *              每个jvm只需要装载自己的应用类即可，好处是：启动时间减少了，另外核心类是共享的，所以jvm的内存占用也减少了。
+ *          可终止的G1 Mixed GC:
+ *              简而言之，当G1垃圾回收器的回收超过暂停时间的目标，则能终止垃圾回收过程
+ *          支持unicode11:
+ *          支持压缩数字格式化：
+ *              NumberFormat添加了对紧凑形式格式化数字的支持，
+ *          String新增方法:
+ *              String#transform(Function):返回该函数返回的输出
+ *              String#indent(int n):提供空格符号
+ *          Files新增mismatch方法:
+ *          新增项
+ *          废弃项
+ *          移除项
+ *
+ */
+
+
+/**
+ * JDK13的新特性
+ *      5个JEP(JAVA的增强提案)
+ *          1：动态CDS档案（CDS:类数据共享）
+ *          2：ZGC:取消使用未使用的内存
+ *          3：重新实现旧版本套接字API
+ *          4：swithc表达式
+ *          5：文字块
+ *
+ *          switch表达式（预览版）
+ *              对比jdk12,引入了yield语句，用于返回值，这意味着，switch表达式的返回值应该使用yield,如果不返回，则使用break;
+ *          文本块：
+ *              文本块就是指多行字符串，例如一段格式化之后的json,xml,有了文本块之后用户就不需要转义，java可以自动给搞定
+ *              文本块可以提高java程序的可读性和可写性
+ *              文本块是java语言的一种新文字，它可以用来表示任何字符串，并且提供更大的表现力和更小的复杂性
+ *              文本块由零个或者多个字符组成，由开始和结束分隔符括起来
+ *              """   """表示开始和结束分隔符,开始符号单独占一行
+ *              文本块中的内容可以直接使用”，“，但不是必须的
+ *              文本块中的内容可以直接包含行终止符，允许再文本块中使用\n，但不是必须的
  *
  */
